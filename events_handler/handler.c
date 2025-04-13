91421c2e57aba3e5b66b8764255fb02f22339d88
Jaiane Guimarães <dsjaiane.guimaraes@gmail.com>
2025-04-13 13:21:11 -0300

    norminettw

--- events_handler/handler.c
+++ events_handler/handler.c
@@ -1,15 +1,27 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   handler.c                                          :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: jaiane <marvin@42.fr>                      +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/04/13 13:19:56 by jaiane            #+#    #+#             */
+/*   Updated: 2025/04/13 13:19:58 by jaiane           ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
 #include "../fdf.h"
 
-int event_handler(int keysym, t_data *data)
+int	event_handler(int keysym, t_data *data)
 {
-    if (keysym == XK_Escape)
-        close_window(data);
-    zoom(keysym, data);
-    scale(keysym, data);
-    rotate(keysym, data);
-    translate(keysym, data);
-    reset(keysym, data);
-    mlx_clear_window(data->mlx.connection, data->mlx.window);
-    draw(data);    
-    return (0);
+	if (keysym == XK_Escape)
+		close_window(data);
+	zoom(keysym, data);
+	scale(keysym, data);
+	rotate(keysym, data);
+	translate(keysym, data);
+	reset(keysym, data);
+	mlx_clear_window(data->mlx.connection, data->mlx.window);
+	draw(data);
+	return (0);
 }
--- events_handler/window_events.c
+++ events_handler/window_events.c
@@ -1,12 +1,24 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   window_events.c                                    :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: jaiane <marvin@42.fr>                      +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/04/13 13:19:44 by jaiane            #+#    #+#             */
+/*   Updated: 2025/04/13 13:19:48 by jaiane           ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
 #include "../fdf.h"
 
 int	close_window(t_data *data)
 {
-	    mlx_destroy_window(data->mlx.connection, data->mlx.window);
-		mlx_destroy_display(data->mlx.connection);
-		free(data->mlx.connection);
-		free_matrix(data->fdf.z_value_m, data->fdf.height);
-		free_matrix(data->fdf.z_color_m, data->fdf.height);
-		exit(1);
-        return (0);
+	mlx_destroy_window(data->mlx.connection, data->mlx.window);
+	mlx_destroy_display(data->mlx.connection);
+	free(data->mlx.connection);
+	free_matrix(data->fdf.z_value_m, data->fdf.height);
+	free_matrix(data->fdf.z_color_m, data->fdf.height);
+	exit(1);
+	return (0);
 }
