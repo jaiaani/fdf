469eff936d5479245b326f4f8f115a4f864b5fa1
Jaiane Guimarães <dsjaiane.guimaraes@gmail.com>
2025-04-13 18:12:35 -0300

    matrix of points

--- fdf_matrix/helpers.c
+++ fdf_matrix/helpers.c
@@ -6,42 +6,44 @@
 /*   By: jaiane <marvin@42.fr>                      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/04/13 11:43:15 by jaiane            #+#    #+#             */
-/*   Updated: 2025/04/13 11:43:19 by jaiane           ###   ########.fr       */
+/*   Updated: 2025/04/13 18:11:41 by jaiane           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../fdf.h"
 
-int	count_tokens(int fd)
+void	fdf_width(t_fdf *fdf, char *line)
 {
-	char	*line;
-	char	**split;
-	int		tokens;
+	int	i;
 
-	tokens = 0;
-	line = get_next_line(fd);
-	split = ft_split(line, ' ');
-	tokens = count_elements(split);
-	free(line);
-	free_split(split);
-	free_static_buffer();
-	return (tokens);
+	i = 0;
+	fdf->width = 0;
+	while (line[i])
+	{
+		while (line[i] && line[i] == ' ')
+			i++;
+		if (line[i])
+			fdf->width++;
+		while (line[i] && line[i] != ' ')
+			i++;
+	}
 }
 
-int	count_lines(int fd)
+void	fdf_init(t_fdf *fdf, int fd)
 {
 	char	*line;
-	int		lines;
 
-	lines = 0;
+	fdf->height = 1;
+	line = get_next_line(fd);
+	if (!line)
+		return ;
+	fdf_width(fdf, line);
 	while (1)
 	{
 		line = get_next_line(fd);
 		if (!line)
 			break ;
 		free(line);
-		lines++;
+		fdf->height++;
 	}
-	close(fd);
-	return (lines);
 }
